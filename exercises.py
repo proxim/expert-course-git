2e928e2787254154875a647e5253d6aef4d97710
kaiyuanli7 <kaiyuanli07@gmail.com>
2023-07-25 13:08:24 -0700

    updated anagrams

--- exercises.py
+++ exercises.py
@@ -26,11 +26,13 @@ def is_anagram(str1, str2):
     if(len(str1) == len(str2)):
         sorted_str1 = sorted(str1)
         sorted_str2 = sorted(str2)
-        print(sorted_str1)
-    if(sorted_str1 == sorted_str2):
-        print("these two words are anagrams")
+        if(sorted_str1 == sorted_str2):
+            return True
+        else:
+            return False
     else:
-        print("these two words are not anagrams")
+        return False
+    
 
 
 def count_vowels(str):
